f83adc0349a63db6ebff40ee360984dd2e3af35a
unknown <dusandjordjevic.work@gmail.com>
2019-05-14 04:16:00 +0200

    authentication commit

---
+++ app/models/shot.rb
@@ -0,0 +1,5 @@
+class Shot < ApplicationRecord
+  belongs_to :user
+
+  mount_uploader :user_shot, UserShotUploader
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  resources :shots
+
+  devise_for :users, controllers: { registrations: 'registrations' }
+
+  root 'shots#index'
+
+end
